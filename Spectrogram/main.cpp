051d536a0dab32c43cc2bbae6e46380de06d6665
ShadowOfDen <31400073+ShadowOfDen@users.noreply.github.com>
2022-11-28 01:30:48 +0400

    Update main.cpp

--- Spectrogram/main.cpp
+++ Spectrogram/main.cpp
@@ -2,59 +2,60 @@
 
 int main()
 {
-    utils::logging::setLogLevel(utils::logging::LOG_LEVEL_ERROR);  // Отключаем логи от openCV (Оставляем только ошибки)
+    utils::logging::setLogLevel(utils::logging::LOG_LEVEL_ERROR);  // РћС‚РєР»СЋС‡Р°РµРј Р»РѕРіРё РѕС‚ openCV (РћСЃС‚Р°РІР»СЏРµРј С‚РѕР»СЊРєРѕ РѕС€РёР±РєРё)
 
     string fileName = "";
 
-    vector<double> data_ch1;  // Вектор значений Левого канала
-    vector<double> data_ch2;  // Вектор значений Правого канала
+    vector<double> data_ch1;  // Р’РµРєС‚РѕСЂ Р·РЅР°С‡РµРЅРёР№ Р›РµРІРѕРіРѕ РєР°РЅР°Р»Р°
+    vector<double> data_ch2;  // Р’РµРєС‚РѕСЂ Р·РЅР°С‡РµРЅРёР№ РџСЂР°РІРѕРіРѕ РєР°РЅР°Р»Р°
 
-    wav_header_t header;  // Создаем объект структуры
-    int samples_count = 0;  // Колличества данных в data
+    wav_header_t header;  // РЎРѕР·РґР°РµРј РѕР±СЉРµРєС‚ СЃС‚СЂСѓРєС‚СѓСЂС‹
+    int samples_count = 0;  // РљРѕР»Р»РёС‡РµСЃС‚РІР° РґР°РЅРЅС‹С… РІ data
 
     cout << "Input the path to the file: "; cin >> fileName;
 
-    // Время выполнения кода
+    // Р’СЂРµРјСЏ РІС‹РїРѕР»РЅРµРЅРёСЏ РєРѕРґР°
     system_clock::time_point start_time, end_time_read, end_time_cpu, end_time_gpu, end_time;
 
-    start_time = system_clock::now(); // Началь времени GPU
+    start_time = system_clock::now(); // РќР°С‡Р°Р»СЊ РІСЂРµРјРµРЅРё GPU
 
-    wav_Reader(header, fileName, samples_count, data_ch1, data_ch2);  // Получаем данные со звука
+    wav_Reader(header, fileName, samples_count, data_ch1, data_ch2);  // РџРѕР»СѓС‡Р°РµРј РґР°РЅРЅС‹Рµ СЃРѕ Р·РІСѓРєР°
 
-    end_time_read = system_clock::now(); // Началь времени GPU
+    end_time_read = system_clock::now(); // РќР°С‡Р°Р»СЊ РІСЂРµРјРµРЅРё GPU
     cout << outputTime("Data read execution time: ", start_time, end_time_read) << endl;
 
-    // Получаем спектр
+    // РџРѕР»СѓС‡Р°РµРј СЃРїРµРєС‚СЂ
     if (processor == 0)
     {
         spectrogram_from_signal(header, samples_count, data_ch1, data_ch2);  // CPU
 
         end_time_cpu = system_clock::now();
-        cout << outputTime("Data read execution time: ", end_time_read, end_time_cpu) << endl;
+        cout << outputTime("The execution time of the Fourier transform CPU: ", end_time_read, end_time_cpu) << endl;
     }
     else if (processor == 1)
     {
         spectrogram_from_signal_cuda(header, samples_count, data_ch1, data_ch2);  // GPU
 
         end_time_gpu = system_clock::now();
-        cout << outputTime("Data read execution time: ", end_time_read, end_time_gpu) << endl;
+        cout << outputTime("The execution time of the Fourier transform GPU: ", end_time_read, end_time_gpu) << endl;
     }
     else
     {
         spectrogram_from_signal(header, samples_count, data_ch1, data_ch2);  // CPU
 
         end_time_cpu = system_clock::now();
-        cout << outputTime("Data read execution time: ", end_time_read, end_time_cpu) << endl;
+        cout << outputTime("The execution time of the Fourier transform CPU: ", end_time_read, end_time_cpu) << endl;
 
         spectrogram_from_signal_cuda(header, samples_count, data_ch1, data_ch2);  // GPU
 
         end_time_gpu = system_clock::now();
-        cout << outputTime("Data read execution time: ", end_time_cpu, end_time_gpu) << endl;
+        cout << outputTime("The execution time of the Fourier transform GPU: ", end_time_cpu, end_time_gpu) << endl;
     }
 
+    end_time = system_clock::now();
     cout << outputTime("Program execution time: ", start_time, end_time) << endl;
 
-    waitKey();  // Ожидаем нажатия клавиш на графиках
+    waitKey();  // РћР¶РёРґР°РµРј РЅР°Р¶Р°С‚РёСЏ РєР»Р°РІРёС€ РЅР° РіСЂР°С„РёРєР°С…
 
     return 0;
 }
